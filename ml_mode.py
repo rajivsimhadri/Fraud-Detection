945eb6731413bb3eb2313350f6172d6e59bb6019
rajivsimhadri <rajiv.simhadri@gmail.com>
2021-10-30 12:40:58 +0530

    added 3 new ml model codes

--- ml_mode.py
+++ ml_mode.py
@@ -1,2 +1,11 @@
 
 print("adding all ml related codes here")
+
+def model1():
+    print("adding code to ml model1")
+
+def model2():
+    print("adding code to ml model2")
+
+def model3():
+    print("adding code to ml model3")
