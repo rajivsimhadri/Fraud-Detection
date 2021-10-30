1ad72498b90aad817c1630439c7d931fdbdec8c8
rajivsimhadri <rajiv.simhadri@gmail.com>
2021-10-30 11:50:05 +0530

    added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,8 @@
 print("adding all the data processing code related to fraud detection")
 
+a=5
+b=6
+def sum(a,b):
+    c=a+b
+    return a
+print("Added a new sum function which is not correct")
--- new_file.py
+++ new_file.py
@@ -1,1 +1,8 @@
 print("new line of life is not a wife")
+
+a=5
+b=6
+def sum(a,b):
+    c=a+b
+    return a
+print("Added a new sum function which is not correct")
